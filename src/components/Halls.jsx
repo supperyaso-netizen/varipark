d11f5dd9fe9550adcfcf167c0ecab610bf2ee0b0
Yaso <supperyaso@gmail.com>
2026-09-02 17:55:46 +0530

    Replace Hall 2 image with minihall.jpg

--- src/components/Halls.jsx
+++ src/components/Halls.jsx
@@ -2,7 +2,7 @@ import { useRef } from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
 import { useInView } from '../hooks/useInView'
 import hall1Img from '../assets/hall1.jpg'
-import hall2Img from '../assets/bar2.jpg'
+import hall2Img from '../assets/minihall.jpg'
 
 const halls = [
   {
